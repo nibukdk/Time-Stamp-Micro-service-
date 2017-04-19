d4376e9824cd48a5b9a018e6d0f8317d61b7cf73
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-04-19 15:52:19 +0000

    Body parser installed and also modification in routes

--- UnixTimeStamp/app.js
+++ UnixTimeStamp/app.js
@@ -1,9 +1,19 @@
-var express= require("express");
-var app= express();
+var express= require("express"),
+    app= express(),
+    moment = require("moment");
+    moment().format();
+    
+
+app.set('view engine','ejs');
 
 
-
-
+app.get('/',function(req,res){
+    res.render('index');
+});
+app.get('/:id',function(req, res){
+    res.send('afjkabfjba');
+    console.log(req.body);
+});
 
 app.listen(process.env.PORT, process.env.IP,function(){
     console.log('The server is running');
